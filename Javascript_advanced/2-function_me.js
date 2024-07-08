d90a399377774a9dbd15a24104d958bb94da966e
Dom <118311759+DominickKeeling@users.noreply.github.com>
2024-07-08 10:02:59 -0500

    Delete Javascript_advanced directory

--- Javascript_advanced/2-function_me.js
+++
@@ -1,9 +0,0 @@
-function welcomeMessage(fullName) {
-  return function() {
-    alert("Welcom " + fullName);
-  }
-}
-
-const guillaume = welcomeMessage("Guillaume");
-const alex = welcomeMessage("Alex");
-const fred = welcomeMessage("Fred");
